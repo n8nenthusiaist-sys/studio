001391759d96581dad2bb5619437fbcf5ede8d7c
Александър Калинов <aleksandrkalinov80@gmail.com>
2025-10-19 10:30:55 +0000

    Switch the video in the hero section with this one https://www.loom.com/

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -20,7 +20,7 @@ export function Header() {
         </div>
         <div id="loom-video" className="relative mt-12 w-full max-w-4xl aspect-video">
           <iframe
-            src="https://www.loom.com/embed/6ec4c92ec0224256ba814b2528fff751"
+            src="https://www.loom.com/embed/10c44262fd2b4d75a69a9229e7c97a3f"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             className="rounded-lg border shadow-2xl absolute top-0 left-0 w-full h-full"
